e29e38779ce441ee7f346c401cae880c34fe1f08
andela-eisaac <deejecks@live.com>
2016-06-19 12:13:29 +0100

    Add more tests to hamper item creation

--- blueprints/hamper/tests/test_models.py
+++ blueprints/hamper/tests/test_models.py
@@ -27,7 +27,7 @@ class ItemTestCase(TestCase):
 
 class HamperTestCase(TestCase):
     def setUp(self):
-        models.Hamper.objects.create(name='Valentine special', description='Something for loved ones')
+        models.Hamper.objects.create(name='Valentine special', description='Something for loved ones',)
 
     def test_hamper_creation(self):
         hamper = models.Hamper.objects.get(name='Valentine special')
@@ -35,16 +35,30 @@ class HamperTestCase(TestCase):
         self.assertTrue(hamper.__str__(), 'Valentine special')
 
 
-class HamperItemJoinTestCase(TestCase):
+class ItemHamperLineTestCase(TestCase):
     def setUp(self):
         category = models.Category.objects.create(name="breakables", description="These items can break")
         hamper = models.Hamper.objects.create(name='Valentine special', description='Something for loved ones')
         item = models.Item.objects.create(name='some gift', description='foo-bar', cost=189.382, category=category)
-        models.ItemHamper.objects.create(item=item, hamper=hamper, item_quantity=5)
+        models.ItemHamperLine.objects.create(item=item, hamper=hamper, item_quantity=5)
 
     def test_item_hamper_join_creation(self):
-        item_hamper = models.ItemHamper.objects.get(item__id=1, hamper__id=1)
-        self.assertTrue(isinstance(item_hamper, models.ItemHamper))
+        item_hamper = models.ItemHamperLine.objects.get(item__name='some gift', hamper__name='Valentine special')
+        self.assertTrue(isinstance(item_hamper, models.ItemHamperLine))
         self.assertTrue(isinstance(item_hamper.item, models.Item))
         self.assertTrue(isinstance(item_hamper.hamper, models.Hamper))
         self.assertTrue(type(item_hamper), int)
+
+    def test_hamper_items(self):
+        hamper = models.Hamper.objects.get(name="Valentine special")
+        hamper_items = hamper.items.all()
+        self.assertTrue(type(hamper_items), list)
+        self.assertTrue(len(hamper_items), 1)
+        self.assertTrue(isinstance(hamper_items[0], models.Item))
+
+    def test_items_and_their_hamper(self):
+        item = models.Item.objects.get(name='some gift')
+        items_hampers = item.hamper_set.all()
+        self.assertTrue(type(items_hampers), list)
+        self.assertTrue(len(items_hampers), 1)
+        self.assertTrue(isinstance(items_hampers[0], models.Hamper))
